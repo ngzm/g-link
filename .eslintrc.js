5a6d1a50f2aa8fc766e80e5ef049b701c526e6be
naoki-nagazumi <naoki.nagazumi@ntt-at.co.jp>
2017-09-26 15:01:29 +0900

    First Commit to start this Project

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  },
+  "globals": {
+    "window": false,
+    "process": false,
+    "module": false,
+    "require": false,
+    "__dirname": false,
+  },
+};
