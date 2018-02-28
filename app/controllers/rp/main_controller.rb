9b6a93628622c5ef3eca89c19a82e9d80890324f
ngzm <nk.ngzm@gmail.com>
2018-02-28 17:46:18 +0900

    Avoid requesting twice to auth create view
    This resolves #56 issue.
    It is caused by TurboLinks,in case of redirecting on the next view.

--- app/controllers/rp/main_controller.rb
+++ app/controllers/rp/main_controller.rb
@@ -1,12 +1,6 @@
 module Rp
   # User authentication main class
   class MainController < RpController
-    def index
-      @gp = '/rp/google/create'
-      @fp = '/rp/facebook/create'
-      @tp = '/rp/twitter/create'
-      @p1 = "client_token=#{@client_token}"
-      @p2 = "redirect_uri=#{@redirect_uri}"
-    end
+    def index; end
   end
 end
