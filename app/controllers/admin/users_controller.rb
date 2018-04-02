9b2296f38750869338146d34543d9ffb7e292a0f
ngzm <nk.ngzm@gmail.com>
2018-04-02 20:30:42 +0900

    Add user admin menu for administators
    This commit fix #62

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,25 @@
+module Admin
+  #
+  # Admin users controller
+  #
+  class UsersController < AdminController
+    before_action :set_user, only: :show
+
+    # GET /users
+    # GET /users.json
+    def index
+      @users = User.all
+    end
+
+    # GET /users/1
+    # GET /users/1.json
+    def show; end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+  end
+end
