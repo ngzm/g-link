317632a8f75061f6a6e0991f3d3a1e10b35c1cd5
Naoki Nagazumi <nk.ngzm@gmail.com>
2018-05-06 16:42:13 +0900

    update client js packeges.
    1. npm outdated
    2. npm update
    3. vue init webpack-simple [hogehoge] (other new project for reference)
    4. Copy from [hogehoge]
    5. update some
    6. update vuetify

--- client/src/components/rank/RankingList.vue
+++ client/src/components/rank/RankingList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-data-table dark :headers="headers" :items="items" hide-actions class="elevation-1">
-    <template slot="items" scope="props">
+    <template slot="items" slot-scope="props">
       <tr @click="onSelect(props.item.cid, props.item.id)">
       <td class="text-xs-right">{{ props.index + 1 }}</td>
       <td class="no_wrap">{{ props.item.title }}</td>
