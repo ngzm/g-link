6a9b815d5912f74fcbefeee29ece005fb236e0c9
ngzm <nk.ngzm@gmail.com>
2018-06-18 16:44:08 +0900

    Add contact confirm and complete view.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,19 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+
+  # Mailer Settings for Sakura Mailbox
+  config.action_mailer.default_url_options = {
+    host: 'freegame.link', port: 443
+  }
+  config.action_mailer.delivery_method = :smtp
+
+  # Mailer SMTP Settings
+  smtp_settings = YAML.load_file(
+    File.expand_path('smtp_settings.yml', __dir__)
+  ).symbolize_keys
+  config.action_mailer.smtp_settings = smtp_settings
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
