1a45a29de57441dd868724fc9215f2449dc3a39d
ngzm <nk.ngzm@gmail.com>
2020-06-07 11:39:17 +0900

    Setup to france site on aws.

--- lib/auths/config.rb
+++ lib/auths/config.rb
@@ -3,22 +3,22 @@ module Auths
   module Config
     def gconf
       @@gconf ||= YAML.load_file(
-        # File.join(Rails.root, 'config/rp_config_google.yml')
-        File.join(Rails.root, 'config/rp_config_google_ngzm.yml')
+        File.join(Rails.root, 'config/rp_config_google.yml')
+        # File.join(Rails.root, 'config/rp_config_google_ngzm.yml')
       )
     end
 
     def fconf
       @@fconf ||= YAML.load_file(
-        # File.join(Rails.root, 'config/rp_config_facebook.yml')
-        File.join(Rails.root, 'config/rp_config_facebook_ngzm.yml')
+        File.join(Rails.root, 'config/rp_config_facebook.yml')
+        # File.join(Rails.root, 'config/rp_config_facebook_ngzm.yml')
       )
     end
 
     def tconf
       @@tconf ||= YAML.load_file(
-        # File.join(Rails.root, 'config/rp_config_twitter.yml')
-        File.join(Rails.root, 'config/rp_config_twitter_ngzm.yml')
+        File.join(Rails.root, 'config/rp_config_twitter.yml')
+        # File.join(Rails.root, 'config/rp_config_twitter_ngzm.yml')
       )
     end
   end
